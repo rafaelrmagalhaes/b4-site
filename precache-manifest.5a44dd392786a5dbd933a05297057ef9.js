217d7f17d50e7ac121dee2325608699c7a7f2a40
Rafael Magalhães <rafael.magalhaes@corp.globo.com>
2020-02-10 22:19:29 -0300

    Updates

---
+++ precache-manifest.5a44dd392786a5dbd933a05297057ef9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f214b865d07e5112cb22075516dcbbe8",
+    "url": "/b4-site/index.html"
+  },
+  {
+    "revision": "856daef0a3243531a135",
+    "url": "/b4-site/static/css/main.07cce1aa.chunk.css"
+  },
+  {
+    "revision": "7092de5cc731f00c1aa2",
+    "url": "/b4-site/static/js/2.82873509.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/b4-site/static/js/2.82873509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856daef0a3243531a135",
+    "url": "/b4-site/static/js/main.a27694aa.chunk.js"
+  },
+  {
+    "revision": "80784a07afebb1c081a8",
+    "url": "/b4-site/static/js/runtime-main.6272b07f.js"
+  },
+  {
+    "revision": "18e979100e4fa97ec4960bde19859bc5",
+    "url": "/b4-site/static/media/banner-fortnite.18e97910.jpg"
+  },
+  {
+    "revision": "06efaa7860df362a3a76e5d43e188836",
+    "url": "/b4-site/static/media/banner-free-fire.06efaa78.jpg"
+  },
+  {
+    "revision": "350a9e5a69fba48e93b7e302fe09e2f7",
+    "url": "/b4-site/static/media/banner-pubg.350a9e5a.jpg"
+  },
+  {
+    "revision": "c3c6be945a12c8de70c2794cdc905848",
+    "url": "/b4-site/static/media/banner.c3c6be94.jpg"
+  }
+]);
